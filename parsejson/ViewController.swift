bb6fdc96df0fad1062845ae2f4015f52ffdc8405
Chip <chip@chips-macbook-pro.local>
2018-03-18 09:55:57 -0500

    Parse Json

--- parsejson/ViewController.swift
+++ parsejson/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var tableArray = [String] ()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        parseJSON()
+        
+        
     }
+    
+    func parseJSON () {
+        
+        let url = URL(string: "https://api.github.com/user/repos")
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error ) in
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            
+        guard error == nil else {
+            print("returned error")
+            return
+        }
+
+        guard let content = data else {
+            print("No data")
+            return
+        }
+            
+            
+        
+        guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+            print("Not containing JSON")
+            return
+        }
+            
+            
+            print(json)
+   
+
+        if let array = json["current_user_repositories_url"] as? [String] {
+            self.tableArray = array
+        } else {
+            
+            print("Json is not loading")
+            
+            }
+        
+        print(self.tableArray)
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+        
+        }
+
+        task.resume()
     }
-
-
+    
 }
+
+    extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.tableArray[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.tableArray.count
+
+        
+        
+    }
+    
+}
+
+   
+
+
 
